b3cd170c0255b8a001739b542b79ab0d92bdfd17
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-23 12:04:10 -0300

    Adicionando tarefas ao estado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 import Navbar from "./components/Navbar/Navbar";
 import TaskList from "./components/TaskList/TaskList";
 
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
+
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+  
+  
   return (
     <div className="App">
       <div>
         <Navbar />
         <div className="container">
-          <TaskList title="Pendente"/>
-          <TaskList title="Fazendo"/>
-          <TaskList title="Concluído" />
+          <TaskList 
+            title="Pendente" 
+            onAddTask={addTask}
+            tasks={tasks}
+          />
         </div>
       </div>
     </div>
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,15 +2,29 @@ import React from "react";
 import "./tasklist.css";
 import PropTypes from "prop-types";
 
-export default function TaskList({title}) {
+export default function TaskList({ title, onAddTask, tasks }) {
+  const addTask = () => {
+    onAddTask("Nova Tarefa", "Pendente");
+  };
+  
   return (
     <div className="tasklist">
-      <div className="title">{title}</div>
-      <div className="content"></div>
+      <div className="title">{title}
+        <div className="content">
+        { tasks.map((task) => (
+          <div key={ task.id }>
+            Teste
+          </div>
+        ))}
+        </div>
+        <button onClick={addTask}>Adicionar nova tarefa</button>
+      </div> 
     </div>
   )
 }
 
 TaskList.propTypes = {
   title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired,
 };
