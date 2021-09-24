9c7e9af7be3c65513894c843b0ecbf2c2bc75328
jhenriquemargrafm <jh.margraf@gmail.com>
2021-09-24 12:04:16 -0300

    Editable Title - Hooks from the Component

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./taskitem.css";
 
 export default function TaskItem({ id, title, taskState }) {
-  return <div>{title}</div>
+  const [isEditing, setIsEditing] = useState(false);
+  const [editableTitle, setEditableTitle] = useState(title);
+
+  const onTitleChange = (event) => {
+    const newTitle = event.target.value;
+    setEditableTitle(newTitle);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.key === "Enter") {
+      setIsEditing(false);
+      }
+  }
+  
+  if (isEditing) {
+    return (
+      <input
+        type="text"
+        value={editableTitle}
+        onChange={onTitleChange}
+        onKeyPress={onKeyPress}
+      />
+    );
+  } else {
+    return (
+      <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+    );
+  }
 }
 
 TaskItem.propTypes = {
